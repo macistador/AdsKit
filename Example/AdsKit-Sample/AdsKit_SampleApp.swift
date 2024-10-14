3606dab36aece6a0417ce31cd15acdbd8467643a
Michel-André Chirita <michel.chirita@gmail.com>
2024-10-14 17:58:02 +0200

    first commit

---
+++ Example/AdsKit-Sample/AdsKit_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdsKit_SampleApp.swift
+//  AdsKit-Sample
+//
+//  Created by Michel-Andre Chirita on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct AdsKit_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
